436c784a17a7be7fe82cc2e1926a30559191db4a
GokulSinghGariya <gokul.hestabit@gmail.com>
2024-02-22 12:28:44 +0530

    payment succes page

---
+++ frontend/pages/payment/cancel.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'user',
+    middleware: 'auth'
+  })
+  
+</script>
---
+++ frontend/pages/payment/status.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'user',
+    middleware: 'auth'
+  })
+  const route = useRoute()
+console.log('routes', route.query)
+</script>
